dd31d0bce9fa1cfc48154b682fb97c2ef61484fa
daklauss <d.klauss@fz-juelich.de>
2024-07-15 16:49:35 +0200

    pipelining
    happening
    
    adding a pipeline.yml for testing with pytest on push
    
    adding ruff.yml wip
    
    addded a ruff workflow
    
    added python 3.12 to the pipeline to check when
    sundials and numpy are working

--- tests/test_residual.py
+++ tests/test_residual.py
@@ -5,8 +5,12 @@ from CADETPythonSimulator.exception import CADETPythonSimError
 import pytest
 import numpy as np
 
+"""
+q_in q_out, was sind physikalisch sinnvolle szenarien
+"""
 
-TestCaseConc = {
+# random number test
+TestCaseConc_level1 = {
     "values" : {
         "c" : np.array([1, 2, 3]),
         "c_dot" : np.array([4, 5, 6]),
@@ -19,12 +23,71 @@ TestCaseConc = {
     "expected" : np.array([-11,-7,-3])
 }
 
+# flow in and out are equal, concentrations to
+TestCaseConc_equal = {
+    "values" : {
+        "c" : np.array([0.1,]),
+        "c_dot" : np.array([0,]),
+        "V" : 1,
+        "V_dot" : 0,
+        "Q_in" : 1,
+        "Q_out" : 1,
+        "c_in" : np.array([0.1,])
+    },
+    "expected" : np.array([0,])
+}
+
+# flow in and out are equal, but concentrations going into the unit is not 
+TestCaseConc_diffcin = {
+    "values" : {
+        "c" : np.array([0.1,]),
+        "c_dot" : np.array([0,]),
+        "V" : 1.0,
+        "V_dot" : 0.0,
+        "Q_in" : 1.0,
+        "Q_out" : 1.0,
+        "c_in" : np.array([0.2,])
+    },
+    "expected" : np.array([-0.1,])
+}
+
+#flow in and out are not equal, concentrantions going in are 
+TestCaseConc_diffvol = {
+    "values" : {
+        "c" : np.array([0.1,]),
+        "c_dot" : np.array([0,]),
+        "V" : 1.0,
+        "V_dot" : 1.0,
+        "Q_in" : 2.0,
+        "Q_out" : 1.0,
+        "c_in" : np.array([0.1,])
+    },
+    "expected" : np.array([0,])
+}
+
+#flow in and out are not, equal, concentrations aren't equal too
+TestCaseConc_diffvolc = {
+    "values" : {
+        "c" : np.array([0.1,]),
+        "c_dot" : np.array([0.2,]),
+        "V" : 1.0,
+        "V_dot" : 1.0,
+        "Q_in" : 2.0,
+        "Q_out" : 1.0,
+        "c_in" : np.array([0.2,])
+    },
+    "expected" : np.array([0,])
+}
 
 
 @pytest.mark.parametrize(
     "parameters",
     [
-        TestCaseConc
+        TestCaseConc_level1, 
+        TestCaseConc_equal, 
+        TestCaseConc_diffcin,
+        TestCaseConc_diffvol,
+        TestCaseConc_diffvolc
     ]
 )
 class TestResidualConcCSTR():
@@ -32,12 +95,12 @@ class TestResidualConcCSTR():
 
         param_vec_conc = parameters["values"].values()
 
-        np.testing.assert_equal(calculate_residual_concentration_cstr(*param_vec_conc), parameters["expected"])
+        np.testing.assert_array_almost_equal(calculate_residual_concentration_cstr(*param_vec_conc), parameters["expected"])
 
 
 
 
-
+# random number test
 TestCaseVol = {
     "values" : {
         "V" : 1,
@@ -48,12 +111,59 @@ TestCaseVol = {
     "expected" : 3
 }
 
+# Flow in and out are equal
+TestCaseVol_equal = {
+    "values" : {
+        "V" : 1,
+        "V_dot" : 0,
+        "Q_in" : 1,
+        "Q_out" : 1,
+    },
+    "expected" : 0
+}
 
+# Flow in is larger than out
+TestCaseVol_inge = {
+    "values" : {
+        "V" : 1,
+        "V_dot" : 1,
+        "Q_in" : 2,
+        "Q_out" : 1,
+    },
+    "expected" : 0
+}
+
+# Flow in is lesser than out
+TestCaseVol_inle = {
+    "values" : {
+        "V" : 1,
+        "V_dot" : -1,
+        "Q_in" : 1,
+        "Q_out" : 2,
+    },
+    "expected" : 0
+}
+
+# Residual does not depend on Volumne
+
+TestCaseVol_vol = {
+    "values" : {
+        "V" : 1e10,
+        "V_dot" : 0,
+        "Q_in" : 0,
+        "Q_out" : 0,
+    },
+    "expected" : 0
+}
 
 @pytest.mark.parametrize(
     "parameters",
     [
-        TestCaseVol
+        TestCaseVol,
+        TestCaseVol_equal,
+        TestCaseVol_inge,
+        TestCaseVol_inle,
+        TestCaseVol_vol
     ]
 )
 
