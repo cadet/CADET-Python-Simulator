ed29a853dfaa7eaffb82aa8d4dcaab2a52f44689
daklauss <d.klauss@fz-juelich.de>
2024-08-29 12:34:47 +0200

    Adjustet tests to new structure

--- tests/test_system.py
+++ tests/test_system.py
@@ -1,69 +1,14 @@
 import numpy as np
 import pytest
 
-from test_unit_operation import (
-    InletFixture, OutletFixture, CstrFixture, DeadEndFiltrationFixture
-)
-from CADETPythonSimulator.system_solver import SystemSolver
+from test_solver import SystemFixture
 
-
-# %% System Fixtures
-
-class SystemSolverFixture(SystemSolver):
-    def __init__(self, unit_operations=None, sections=None, *args, **kwargs):
-        if unit_operations is None:
-            unit_operations = [
-                InletFixture(),
-                DeadEndFiltrationFixture(),
-                OutletFixture()
-            ]
-
-        if sections is None:
-            sections = [
-                {
-                    'start': 0,
-                    'end': 10,
-                    'connections': [
-                        [0, 1, 1e-3],
-                        [1, 2, 1e-3],
-                    ],
-                    'section_states': {
-                        'inlet': {
-                            'c': [
-                                [1, 0, 0, 0],
-                                [2, 0, 0, 0],
-                            ],
-                        },
-                    },
-                },
-                {
-                    'start': 10,
-                    'end': 12,
-                    'connections': [
-                        [0, 1, 1e-3],
-                        [1, 2, 0.5e-3],
-                    ],
-                    'section_states': {
-                        'inlet': {
-                            'c': [
-                                [0, 1, 0, 0],
-                                [1, 0, 0, 0],
-                            ],
-                        },
-                    },
-                },
-            ]
-
-        super().__init__(unit_operations, sections, *args, **kwargs)
-
-
-# %% State Structure
 
 @pytest.mark.parametrize(
-    "system_solver, expected",
+    "system, expected",
     [
         (
-            SystemSolverFixture(),
+            SystemFixture(),
             {
                 'n_dof': 16,
                 'unit_slices': {
@@ -139,60 +84,19 @@ class SystemSolverFixture(SystemSolver):
         ),
     ]
 )
-class TestSystemSolverStateStructure():
+class TestSystem():
 
-    def test_state_structure(self, system_solver, expected):
-        """Test the system state."""
-        assert system_solver.n_dof == expected['n_dof']
-        # Transform keys to unit objects
-        expected_slices = {
-            system_solver.unit_operations_dict[unit]: value
-            for unit, value in expected['unit_slices'].items()
-        }
-        np.testing.assert_equal(system_solver.unit_slices, expected_slices)
-
-    def test_solution_recorder(self, system_solver: SystemSolver, expected):
-        """Test the solution recorder."""
-        system_solver.initialize_solution_recorder()
-
-        solution = np.arange(system_solver.n_dof)
-        system_solver.write_solution(solution, 2 * solution)
-        system_solver.write_solution(2 * solution, 4 * solution)
-
-        unit_solutions = system_solver.unit_solutions
-        for unit, solutions in unit_solutions.items():
-            for state, values in solutions.items():
-                np.testing.assert_almost_equal(
-                    values, expected['unit_solution'][str(unit)][state]
-                )
-
-    def test_section_states(self, system_solver, expected):
-        for i_sec, section in enumerate(system_solver.sections):
-            system_solver._update_section_states(
-                section['start'], section['end'], section['section_states']
-            )
-
-            for unit in system_solver.units:
-                expected_values = expected['section_states'][i_sec]
-
-                values_start = unit.get_parameter_values_at_time(section['start'])
-                np.testing.assert_equal(
-                    values_start, expected_values['parameters_start'][str(unit)]
-                )
-
-                values_end = unit.get_parameter_values_at_time(section['end'])
-                np.testing.assert_equal(
-                    values_end, expected_values['parameters_end'][str(unit)]
-                )
+    def test_system_structure(self, system, expected):
+        assert system.n_dof == expected['n_dof']
 
 
 # %% System Connectivity
 
 @pytest.mark.parametrize(
-    "system_solver, connections, expected_matrix, expected_state",
+    "system, connections, expected_matrix, expected_state",
     [
         (
-            SystemSolverFixture(),
+            SystemFixture(),
             [[0, 1, 0, 0, 1e-3], [1, 2, 0, 0, 1e-3]],
             [
                 [0.001, 0.   ],
@@ -208,33 +112,32 @@ class TestSystemSolverStateStructure():
 class TestSystemConnectivity():
     def test_connections_matrix(
             self,
-            system_solver,
+            system,
             connections,
             expected_matrix,
             expected_state
             ):
-        matrix = system_solver._compute_connectivity_matrix(connections)
+        matrix = system._compute_connectivity_matrix(connections)
         np.testing.assert_almost_equal(matrix, expected_matrix)
 
     def test_coupling(
             self,
-            system_solver,
+            system,
             connections,
             expected_matrix,
             expected_state
             ):
 
-        y = np.arange(system_solver.n_dof)
+        y = np.arange(system.n_dof)
         y_dot = 2 * y
 
-        system_solver.couple_unit_operations(connections, y, y_dot)
+        system.couple_unit_operations(connections, y, y_dot)
         np.testing.assert_almost_equal(y, expected_state)
 
 # %% TODO: System Residual
 
 
 
-# %% Run tests
 
 if __name__ == "__main__":
-    pytest.main(["test_system_solver.py"])
+    pytest.main(["test_system.py"])
