252d77e5db35d7e7823a401a7c365074590e9d93
daklauss <d.klauss@fz-juelich.de>
2024-09-05 14:52:26 +0200

    Reworked and implemented new System/Coupling

--- CADETPythonSimulator/coupling_interface.py
+++ CADETPythonSimulator/coupling_interface.py
@@ -4,32 +4,31 @@ from CADETPythonSimulator.unit_operation import UnitOperationBase
 
 
 class CouplingInterface(abc.ABC):
-    @abc.abstractmethod
-    def get_coupled_state(self,
-                          origin_list: list[(dict, float)],
-                          coupling_state_structure: dict
-                          ) -> dict:
-        """Calculates new state for destination_unit"""
+    """Abstract interface for coupling states."""
 
+    @abc.abstractmethod
+    def get_coupled_state(
+        self,
+        origin_list: list[(dict, float)],
+        state: str
+        ) -> np.ndarray:
+        """Calculate new state for destination_unit."""
 
 
 class AverageCoupling(CouplingInterface):
+    """Implements the Coupling Interface for average Coupling."""
 
     def get_coupled_state(self,
                           origin_list: list[(dict, float)],
-                          coupling_state_structure: dict
-                          ) -> dict:
-        ret = {}
-        for state, dim in coupling_state_structure.items():
-            ret[state] = np.zeros(dim)
+                          state: str
+                          ) -> np.ndarray:
+        """Calculate new state for destination_unit with average Coupling."""
+        ret = np.zeros(origin_list[0][0][state].shape)
+        rate_tot = 0
+        for list_state, rate in origin_list:
+            ret += list_state[state]*rate
+            rate_tot += rate
 
-        value_tot = 0
-        for unit, value in origin_list:
-            for state in coupling_state_structure.keys():
-                ret[state] += unit[state]*value
-            value_tot += value
-
-        for value in ret.values():
-            value/=value_tot
+        ret /= rate_tot
 
         return ret
