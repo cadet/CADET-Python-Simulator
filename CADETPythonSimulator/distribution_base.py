79aec6f5d58c98980d38c42981d6ffd48b13a199
daklauss <d.klauss@fz-juelich.de>
2025-03-06 10:30:02 +0100

    Add Concentration distribution

--- CADETPythonSimulator/distribution_base.py
+++ CADETPythonSimulator/distribution_base.py
@@ -1,8 +1,98 @@
 import numpy as np
+import numpy.typing as npt
+from CADETPythonSimulator.componentsystem import CPSComponentSystem
+from CADETPythonSimulator.exception import CADETPythonSimError
 
 class DistributionBase:
-    """Small wrapper class for Concentration."""
+    """Small wrapper class for implementing Distributions for Boundary Conditions."""
 
     def get_distribution(t: float, section_nr: int)->np.ndarray:
         """Abtract class to call."""
         pass
+
+class ConstantVolumeDistribution(DistributionBase):
+    """Implements DistributionBase for a Constant Volume Distribution."""
+
+    def __init__(self, component_system: CPSComponentSystem, c: npt.ArrayLike):
+        """
+        Construct the Object and calculate the Constant Value.
+
+        Concentration of the Last element is not given.
+        """
+        if len(c) != component_system.n_comp - 1:
+            raise ValueError(
+                f"Length of concentrations must be {component_system.n_comp - 1}"
+            )
+
+        self.component_system = component_system
+        self.c = c
+
+        n = [i for i in self.c]
+        m = [
+            n_i * self.component_system.molecular_weights[i]
+            for i, n_i in enumerate(n)
+        ]
+        V = [
+            m_i / self.component_system.pure_densities[i]
+            for i, m_i in enumerate(m)
+        ]
+        V_solvent = 1 - sum(V)
+
+        if V_solvent<0:
+            raise CADETPythonSimError(
+                "Last species Volume is negative. Misconfigured Simulation"
+                )
+
+        V.append(V_solvent)
+
+        self.V_dist = [
+            i / sum(V)
+            for i in V
+            ]
+
+    def get_distribution(self, t, sec) -> list[float]:
+        """Return Constant Volume Distribution."""
+        return self.V_dist
+
+
+class ConstantConcentrationDistribution(DistributionBase):
+    """Implements of DistributionBase for a Constant Concentration Distribution."""
+
+    def __init__(self, component_system: CPSComponentSystem, c: npt.ArrayLike):
+        """
+        Construct the object and calculate the Constant Value.
+
+        Concentration of the Last element is not given.
+        """
+        if len(c) != component_system.n_comp - 1:
+            raise ValueError(
+                f"Length of concentrations must be {component_system.n_comp - 1}"
+            )
+
+        self.component_system = component_system
+
+        n = [i for i in c]
+        m = [
+            n_i * component_system.molecular_weights[i]
+            for i, n_i in enumerate(n)
+        ]
+        V = [
+            m_i / component_system.pure_densities[i]
+            for i, m_i in enumerate(m)
+        ]
+        V_solvent = 1 - sum(V)
+
+        if V_solvent<0:
+            raise CADETPythonSimError(
+                "Last species Volume is negative. Misconfigured Simulation"
+                )
+
+        m_solvent = component_system.pure_densities[-1] * V_solvent
+        n_solvent = m_solvent / component_system.molecular_weights[-1]
+
+        self.c = [*c, n_solvent]
+
+
+    def get_distribution(self, t, sec) -> list[float]:
+        """Return Constant Concentrations."""
+        return self.c
---
+++ tests/test_distribution.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pytest
+
+from CADETPythonSimulator.componentsystem import CPSComponentSystem
+from CADETPythonSimulator.distribution_base import (
+    ConstantVolumeDistribution, ConstantConcentrationDistribution
+    )
+from CADETPythonSimulator.exception import CADETPythonSimError
+
+component_system = CPSComponentSystem(
+    name="test_comp",
+    components=2,
+    pure_densities=[1, 1],
+    molecular_weights=[1, 2],
+    viscosities=[1, 1],
+    specific_cake_resistances=[1,1]
+    )
+
+@pytest.mark.parametrize(
+    "distribution, component_system, concentration, expected",
+    [
+        (
+            ConstantVolumeDistribution,
+            component_system,
+            [1],
+            [1, 0]
+        ),
+        (
+            ConstantConcentrationDistribution,
+            component_system,
+            [1],
+            [1, 0]
+        )
+    ]
+)
+class TestDistribution:
+    """Test Class for Distribution."""
+
+    def test_getter(self, distribution, component_system, concentration, expected):
+        """Test retunr Function."""
+        dist = distribution(component_system, concentration)
+        assert dist.get_distribution(0, 0) == expected
+
+    def test_error(self, distribution, component_system, concentration, expected):
+        """Test Error Throwing."""
+        with pytest.raises(ValueError):
+            distribution(component_system, [1,1])
+        with pytest.raises(CADETPythonSimError):
+            distribution(component_system, [10000])
